84ae6799e182b72f1c0e221bd91fd280c1f87df1
NataliaWajcowicz <105540039+NataliaWajcowicz@users.noreply.github.com>
2022-12-09 14:47:28 +0000

    Deploying to gh-pages from @ NataliaWajcowicz/goit-js-hw-09@4aeb434fab26481e9c38d959d7e98f1e54624aa3 🚀

---
+++ 01-color-switcher.0484ed1f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.querySelector("body"),o=null;function c(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));e.style.backgroundColor="".concat(t)}t.addEventListener("click",(function(){o=setInterval((function(){c()}),1e3)})),n.addEventListener("click",(function(){clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.0484ed1f.js.map
