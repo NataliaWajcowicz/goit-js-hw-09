4aeb434fab26481e9c38d959d7e98f1e54624aa3
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-12-09 15:47:01 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+let timerId=null;
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor(){
+    const color = getRandomHexColor();
+    body.style.backgroundColor = `${color}`;
+
+
+}
+
+function startClickHandler(){
+        timerId = setInterval(()=>{
+        changeColor()
+    },1000)
+
+}
+
+function stopClickHandler(){
+    clearInterval(timerId);
+
+}
+
+startBtn.addEventListener("click", startClickHandler);
+
+stopBtn.addEventListener("click", stopClickHandler);
+
 
