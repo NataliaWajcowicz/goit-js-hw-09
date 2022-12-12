0854c39a8020f859bcd01ad22a4f317b67077cb7
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-12-12 23:15:06 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,31 +8,35 @@ const userDelay = parseInt(delayInput.textContent);
 const userStep = parseInt(stepInput.textContent);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  return new Promise((resolve,reject)=>{
-  if (shouldResolve) {
-     resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);  
-    } else {
-     reject(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
-  }).then(console.log).catch(console.error)
-  
+  setTimeout(() => {
+    for (let i = 1; i <= userAmount; i++) {
+      const shouldResolve = Math.random() > 0.3;
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (shouldResolve) {
+            resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          } else {
+            reject(`❌ Rejected promise ${position} in ${delay}ms`);
+          }
+        }, userStep)
+      })
+        .then(console.log)
+        .catch(console.error)
+    }
+  }, userDelay);  
 }
 
-function createLoop()  {
-  for (let i = 2; i <= userAmount; i++) {
-    createPromise(i, userStep)
-  }
-}
 
 
-btn.addEventListener("click", () => {
-  createPromise(1, userDelay);
-  createLoop();
-  
+
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  createPromise(userAmount, userStep);
+   
   }
 )
-
+ 
 
 
 
