406da71d83f3ded6927c15f1e73851fb6f80f1f9
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-12-14 01:03:43 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,77 +6,79 @@ const daysLeft = document.querySelector("[data-days]");
 const hoursLeft = document.querySelector("[data-hours]");
 const minutesLeft = document.querySelector("[data-minutes]");
 const secondsLeft = document.querySelector("[data-seconds]");
-const spans = document.querySelectorAll(".value");
+const input = document.querySelector("input");
+let selectedData;
+let timerId;
 
-
-
-
-
-
-
-
+btn.addEventListener("click", remainingTime);
 
 btn.setAttribute("disabled", true);
 
+console.log(input.attributes)
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        function remainingTime() {
-            const data = new Date();
-            const selectedData = selectedDates[0];
-            const timeLeft = selectedData.getTime() -data.getTime();
-            
-
-            if (timeLeft >= 0) {
-                btn.removeAttribute("disabled");
-                
-
-
-                const second = 1000;
-                const minute = second * 60;
-                const hour = minute * 60;
-                const day = hour * 24;
-
-
-
-         
-                const days = Math.floor(timeLeft / day);
-                const hours = Math.floor((timeLeft % day) / hour);
-                const minutes = Math.floor(((timeLeft % day) % hour) / minute);
-                const seconds = Math.floor((((timeLeft % day) % hour) % minute) / second);
-
-                const timerCounter = { days, hours, minutes, seconds };
-
-               
-                daysLeft.textContent= `${days}`.padStart(2,"0");
-                hoursLeft.textContent = `${hours}`.padStart(2,"0");;
-                minutesLeft.textContent = `${minutes}`.padStart(2,"0");;
-                secondsLeft.textContent = `${seconds}`.padStart(2,"0");; 
-                            
-            
-                
-            } else {
-                window.alert("Please choose a date in the future");
-            }
-        }
-
-        remainingTime();
-
-        btn.addEventListener("click", ()=>
-        setInterval(() => { remainingTime() }, 1000))
-
-
-        
-       
-      
-        
-  },
-};
+            btn.removeAttribute("disabled");
+            selectedData = selectedDates[0].getTime();
+       // }
+    }
+}
 
 const calendar = new flatpickr("#datetime-picker", options);
+
+function remainingTime() {
+    btn.setAttribute("disabled", true);
+    input.setAttribute("disabled", true);
+    console.log(input.attributes);
+
+    timerId = setInterval(() => {
+        const data = Date.now();
+        const timeLeft = selectedData - data;
+        const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+        if (timeLeft < 0) {
+            clearInterval(timerId);
+            return window.alert("Please choose a date in the future");
+        }
+
+        updateTimer({ days, hours, minutes, seconds });
+    }, 1000);
+
+    
+};
+
+function updateTimer({ days, hours, minutes, seconds }) {
+    daysLeft.textContent = `${days}`.padStart(2, "0");
+    hoursLeft.textContent = `${hours}`.padStart(2,"0");
+    minutesLeft.textContent = `${minutes}`.padStart(2, "0");
+    secondsLeft.textContent = `${seconds}`.padStart(2, "0");
+    
+}
+
 
 
 
